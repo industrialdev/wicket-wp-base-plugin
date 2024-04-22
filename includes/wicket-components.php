451d3dab043e8c2a20b9a182b9892d7a9fce7897
Stephanie <sducasse@industrialagency.ca>
2024-04-22 09:38:47 -0400

    added wicket components

---
+++ includes/wicket-components.php
@@ -0,0 +1,25 @@
+<?php 
+
+function get_component( $slug, array $args = array(), $output = true ) {
+	/* $args will be available in the component file */
+	if ( ! $output ) {
+		ob_start();
+	}
+	$template_file = locate_template( "components/{$slug}.php", false, false );
+	if ( file_exists( $template_file ) ) :
+		require ( $template_file );
+	else :
+		throw new \RuntimeException( "Could not find component $slug" );
+	endif;
+	if ( ! $output ) {
+		return ob_get_clean();
+	}
+}
+
+function component_exists( $slug ) {
+	$template_file = locate_template( "components/{$slug}.php", false, false );
+	if ( file_exists( $template_file ) ) {
+		return true;
+	}
+	return false;
+}
