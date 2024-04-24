304e7f8e6cc7095ea5eb665d9b2c5df7a528f934
Rob Ferguson <rferguson@wicket.io>
2024-04-24 15:53:45 -0400

    Properly connect the 'connected' status

--- includes/admin/settings/options/wicket-api-check.php
+++ includes/admin/settings/options/wicket-api-check.php
@@ -25,13 +25,22 @@ class WicketAPICheck extends OptionAbstract {
     public function render()
     { 
       $client = wicket_api_client();
+      $wicket_settings = get_wicket_settings();
+      $can_connect = true;
+
+      try {
+        // test the endpoint to ensure it's up
+        $client->get($wicket_settings['api_endpoint']);
+      } catch (\Throwable $th) {
+        $can_connect = false;
+      }
       ?>
       <tr valign="top">
         <th scope="row" class="titledesc">
           <?php echo __('Status', 'wicket'); ?>
         </th>
         <td class="forminp forminp-wicket-api-status">
-          <?php if($client): ?>
+          <?php if($can_connect): ?>
             <span class="wicket-api-status positive"><?php echo __('CONNECTED', 'wicket'); ?></span>
           <?php else: ?>
             <span class="wicket-api-status negative"><?php echo __('NOT CONNECTED', 'wicket'); ?></span>
