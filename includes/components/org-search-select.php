70f16d36aa7ec07407290bdf2199684b0c63bdb7
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-05-30 17:04:23 -0400

    Some progress on the org search select component

--- includes/components/org-search-select.php
+++ includes/components/org-search-select.php
@@ -2,21 +2,28 @@
 $defaults  = array(
 	'classes'               => [],
 );
-$args                  = wp_parse_args( $args, $defaults );
-$classes               = $args['classes'];
-$searchMode            = 'org'; // Options: org, groups, ...
+$args                            = wp_parse_args( $args, $defaults );
+$classes                         = $args['classes'];
+$searchMode                      = 'org'; // Options: org, groups, ...
+$relationshipTypeUponOrgCreation = 'employee';
 
 // TODO: Check current user org relationship to see if they already have a selected org,
 // and if so populate selectedOrgUuid in Apline
 
 ?>
 
-<div class="component-org-search-select " x-data="orgss">
+<div class="container component-org-search-select " x-data="orgss">
 
   <?php // TODO: add conditional CTA displaying the currently selected UUID ?>
 
   <form class="orgss-search-form" x-on:submit="handleSubmit">
-    <input type="text" class="orgss-search-box" x-model="searchBox" />
+    <input type="text" class="orgss-search-box" placeholder="Search by organization name" x-model="searchBox" />
+    <?php get_component( 'button', [ 
+			'variant'  => 'primary',
+			'label'    => __( 'Search', 'wicket' ),
+			'type'     => 'submit',
+			'classes'  => [ '' ],
+		] ); ?>
   </form>
 
 </div>
@@ -25,17 +32,20 @@ $searchMode            = 'org'; // Options: org, groups, ...
 <script>
     document.addEventListener('alpine:init', () => {
         Alpine.data('orgss', () => ({
-            searchType: <?php echo $searchMode; ?>,
+            searchType: '<?php echo $searchMode; ?>',
+            relationshipTypeUponOrgCreation: '<?php echo $relationshipTypeUponOrgCreation; ?>',
             selectedOrgUuid: '',
             searchBox: '',
             results: [],
 
  
-            handleSubmit() {
-              if( searchType == 'org' ) {
-                searchOrgs( searchBox );
-              } else if( searchType == 'groups' ) {
-                searchGroups( searchBox );
+            handleSubmit(e) {
+              e.preventDefault();
+
+              if( this.searchType == 'org' ) {
+                this.searchOrgs( this.searchBox );
+              } else if( this.searchType == 'groups' ) {
+                this.searchGroups( this.searchBox );
               }
             },
             searchOrgs( searchTerm ) {
