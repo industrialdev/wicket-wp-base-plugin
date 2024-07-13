00d8932f5734c402b2bda930d87a614a51b7d2b6
aykan <aykanburcak@gmail.com>
2024-07-13 15:52:03 +0300

    Adding product card component

---
+++ includes/components/card-product.php
@@ -0,0 +1,47 @@
+<?php
+$defaults        = array(
+	'classes'     => [],
+	'post_id'     => '',
+	'title'       => '',
+	'excerpt'     => '',
+	'date'        => '',
+	'link'        => '',
+	'image'       => '',
+	'member_only' => false,
+	'cta'         => null,
+	'cta_label'   => '',
+);
+$args            = wp_parse_args( $args, $defaults );
+$classes         = $args['classes'];
+$product_id      = $args['post_id'];
+$content_type    = $args['content_type'];
+$title           = $args['title'];
+$link            = $args['link'];
+$image           = $args['image'];
+$member_only     = $args['member_only'];
+$cta             = $args['cta'];
+$cta_label       = $args['cta_label'];
+$add_to_cart_url = '';
+
+$classes[]             = 'component-card-product bg-white shadow-4 flex flex-col items-center gap-4 relative';
+$image_wrapper_classes = [];
+$title_classes         = [ 'component-card-product__title block text-dark-100 font-bold leading-7 text-heading-xs' ];
+
+// If woocommerce is not active, return early
+if ( ! class_exists( 'WooCommerce' ) ) {
+	return;
+}
+
+?>
+
+<div class="@container">
+	<div class="<?php echo implode( ' ', $classes ) ?>">
+		<a href="<?php the_permalink(); ?>" class="relative">
+			<?php woocommerce_show_product_loop_sale_flash(); ?>
+			<?php woocommerce_template_loop_product_thumbnail(); ?>
+		</a>
+		<h2 class="component-card-product__title"><?php the_title(); ?></h2>
+		<?php woocommerce_template_loop_price(); ?>
+		<?php woocommerce_template_loop_add_to_cart(); ?>
+	</div>
+</div>
