cc60a59f7607c0e628062e906f24b264694fe1e2
Alex Masliychuk <alex@wicket.io>
2024-10-07 14:33:35 +0300

    Author Block integrated

--- includes/components/author.php
+++ includes/components/author.php
@@ -11,7 +11,11 @@ $author             = $args['author'];
 $hide_profile_image = $args['hide_profile_image'];
 $hide_bio           = $args['hide_bio'];
 
-$classes[] = 'component-author flex flex-col md:flex-row gap-6';
+if ( defined('WICKET_WP_THEME_V2') ) {
+	$classes[] = 'component-author';
+} else {
+	$classes[] = 'component-author flex flex-col md:flex-row gap-6';
+}
 ?>
 
 <div class="<?php echo implode( ' ', $classes ) ?>">
@@ -19,18 +23,19 @@ $classes[] = 'component-author flex flex-col md:flex-row gap-6';
 		$avatar = $author['user_avatar'];
 		$avatar = str_replace( 'width=\'96\'', 'width=\'120\'', $avatar );
 		$avatar = str_replace( 'height=\'96\'', 'height=\'120\'', $avatar );
-
-		echo '<div class="flex-[0_1_120px] min-w-[120px]">';
-		echo $avatar;
-		echo '</div>';
+	?>
+		<div class="<?php echo defined('WICKET_WP_THEME_V2') ? 'component-author__avatar' : 'flex-[0_1_120px] min-w-[120px]' ?>">
+		<?php echo $avatar; ?>
+		</div>
+	<?php
 	} ?>
-	<div class="flex flex-col gap-4">
+	<div class="<?php echo defined('WICKET_WP_THEME_V2') ? 'component-author__info' : 'flex flex-col gap-4' ?>">
 		<?php if ( $author['display_name'] ) : ?>
-			<div class="text-heading-xs font-bold"><?php echo $author['display_name']; ?></div>
+			<div class="<?php echo defined('WICKET_WP_THEME_V2') ? 'component-author__name' : 'text-heading-xs font-bold' ?>"><?php echo $author['display_name']; ?></div>
 		<?php endif; ?>
 
 		<?php if ( $author['user_description'] && ! $hide_bio ) : ?>
-			<div class="text-body"><?php echo $author['user_description']; ?></div>
+			<div class="<?php echo defined('WICKET_WP_THEME_V2') ? 'component-author__bio' : 'text-body' ?>"><?php echo $author['user_description']; ?></div>
 		<?php endif; ?>
 	</div>
 </div>
