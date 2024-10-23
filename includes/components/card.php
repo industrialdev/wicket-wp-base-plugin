79fb2a9b469b0b4e5fa76f2dbd106eb3ad1f0067
Alex Masliychuk <alex@wicket.io>
2024-10-23 17:01:38 +0300

    card component integrated

--- includes/components/card.php
+++ includes/components/card.php
@@ -25,7 +25,11 @@ $link              = $args['link'];
 $cta_style         = $args['cta_style'];
 $image             = $args['image'];
 
-$classes[] = 'component-card p-4 bg-white shadow-4 flex flex-col gap-4 relative @2xl:flex-row @2xl:items-center';
+$classes[] = 'component-card';
+
+if ( ! defined( 'WICKET_WP_THEME_V2' ) ) {
+	$classes[] = 'p-4 bg-white shadow-4 flex flex-col gap-4 relative @2xl:flex-row @2xl:items-center';
+}
 ?>
 
 <div class="@container">
@@ -40,34 +44,40 @@ $classes[] = 'component-card p-4 bg-white shadow-4 flex flex-col gap-4 relative 
 			</div>
 		<?php } ?>
 
-		<div class="component-card__container flex flex-col gap-4 <?php echo $image ? '@2xl:basis-1/2' : '' ?>">
+		<div
+			<?php if ( defined( 'WICKET_WP_THEME_V2' ) ): ?>
+				class="component-card__container <?php echo $image ? 'component-card__container--with-image' : '' ?>"
+			<?php else: ?>
+				class="component-card__container flex flex-col gap-4 <?php echo $image ? '@2xl:basis-1/2' : '' ?>"
+			<?php endif; ?>
+		>
 
 			<?php if ( $pre_heading ) { ?>
-				<div class="block uppercase text-dark-060 font-semibold leading-7 text-body-md @2xl:text-body-lg -mb-3">
+				<div class="<?php echo defined( 'WICKET_WP_THEME_V2' ) ? 'component-card__pre-heading' : 'block uppercase text-dark-060 font-semibold leading-7 text-body-md @2xl:text-body-lg -mb-3' ?>">
 					<?php echo $pre_heading; ?>
 				</div>
 			<?php } ?>
 
 			<?php if ( $title ) { ?>
-				<div class="block text-dark-100 font-bold leading-7 text-heading-xs @2xl:text-heading-md">
+				<div class="<?php echo defined( 'WICKET_WP_THEME_V2' ) ? 'component-card__title' : 'block text-dark-100 font-bold leading-7 text-heading-xs @2xl:text-heading-md' ?>">
 					<?php echo $title; ?>
 				</div>
 			<?php } ?>
 
 			<?php if ( $subtitle && $subtitle_location == 'after-heading' ) { ?>
-				<div class="text-dark-070 italic">
+				<div class="<?php echo defined( 'WICKET_WP_THEME_V2' ) ? 'component-card__subtitle' : 'text-dark-070 italic' ?>">
 					<?php echo $subtitle; ?>
 				</div>
 			<?php } ?>
 
 			<?php if ( $excerpt ) { ?>
-				<div class="leading-6">
+				<div class="<?php echo defined( 'WICKET_WP_THEME_V2' ) ? 'component-card__excerpt' : 'leading-6' ?>">
 					<?php echo $excerpt; ?>
 				</div>
 			<?php } ?>
 
 			<?php if ( $subtitle && $subtitle_location == 'after-excerpt' ) { ?>
-				<div class="text-dark-070 italic">
+				<div class="<?php echo defined( 'WICKET_WP_THEME_V2' ) ? 'component-card__subtitle' : 'text-dark-070 italic' ?>">
 					<?php echo $subtitle; ?>
 				</div>
 			<?php } ?>
