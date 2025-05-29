cc2e2581b06d07e46068f26bb643cf0d17f72a47
Esteban <esteban@attitude.cl>
2025-05-29 16:22:58 -0400

    avoid js collisions, we don't need the same object for widgets over and over again

--- includes/components/widget-prefs-person.php
+++ includes/components/widget-prefs-person.php
@@ -1,18 +1,18 @@
 <?php
-$defaults        = array(
-	'classes'                      => [],
+$defaults        = [
+  'classes'                      => [],
   'hide_comm_prefs'              => false,
   'preferences_data_field_name'  => 'preferences-info',
   'validation_data_field_name'   => 'preferences-validation',
-);
+];
+
 $args                         = wp_parse_args( $args, $defaults );
 $classes                      = $args['classes'];
 $hide_comm_prefs              = $args['hide_comm_prefs'];
 $preferences_data_field_name  = $args['preferences_data_field_name'];
-$unique_widget_id             = rand( 1, 9999999 );
+$unique_widget_id             = rand( 1, PHP_INT_MAX );
 
-$wicket_settings = get_wicket_settings(); 
-
+$wicket_settings = get_wicket_settings();
 ?>
 
 <div class="wicket-section <?php implode( ' ', $classes ); ?>" role="complementary">
@@ -22,29 +22,33 @@ $wicket_settings = get_wicket_settings();
   <?php /* No hidden validation field as the preferences widget doesn't need validation */ ?>
 </div>
 
-<script>
+<script type="text/javascript">
+  if (typeof window.Wicket === 'undefined') {
     window.Wicket=function(doc,tag,id,script){
     var w=window.Wicket||{};if(doc.getElementById(id))return w;var ref=doc.getElementsByTagName(tag)[0];var js=doc.createElement(tag);js.id=id;js.src=script;ref.parentNode.insertBefore(js,ref);w._q=[];w.ready=function(f){w._q.push(f)};return w
     }(document,"script","wicket-widgets","<?php echo $wicket_settings['wicket_admin'] ?>/dist/widgets.js");
+  }
 </script>
 
 <script>
-    (function (){
+    document.addEventListener("DOMContentLoaded", function() {
         Wicket.ready(function () {
-            var widgetRoot = document.getElementById('preferences-widget-<?php echo $unique_widget_id; ?>');
+            let widgetRoot_<?php echo $unique_widget_id; ?> = document.getElementById('preferences-widget-<?php echo $unique_widget_id; ?>');
 
             Wicket.widgets.editPersonPreferences({
-              rootEl: widgetRoot,
+              rootEl: widgetRoot_<?php echo $unique_widget_id; ?>,
               apiRoot: '<?php echo $wicket_settings['api_endpoint'] ?>',
               accessToken: '<?php echo wicket_access_token_for_person(wicket_current_person_uuid()) ?>',
               personId: '<?php echo wicket_current_person_uuid(); ?>',
-              lang: "<?php echo defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE : 'en' ?>",
+              lang: "<?php echo wicket_get_current_language(); ?>",
               hideCommunicationPreferences: <?php echo $hide_comm_prefs ? 'true' : 'false' ?>,
             }).then(function (widget) {
+              <?php
               // Dispatch custom events to the page on each available widget listener,
               // so that actions can be taken based on that information if needed,
               // such as in the Gravity Forms wrapper. Also update hidden fields to
               // make data available in multiple ways on the page
+              ?>
               widget.listen(widget.eventTypes.WIDGET_LOADED, function (payload) {
                 let event = new CustomEvent("wwidget-component-prefs-person-loaded", {
                   detail: payload
@@ -76,5 +80,5 @@ $wicket_settings = get_wicket_settings();
 
           userInfoDataField.value = JSON.stringify(payload);
         }
-    })()
+    });
 </script>
